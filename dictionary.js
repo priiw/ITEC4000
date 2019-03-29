bc90cb3f923ddb461c7928d98a31d8938ae39fcb
priiw <48761109+priiw@users.noreply.github.com>
2019-03-29 16:44:37 -0400

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -24,7 +24,8 @@ class StitchDictionary{
         return this._stitch;
     }
 }
-const knit = new Stitch("knit", "K", "knit", "k", 1);//Knit stitch
-const purl = new Stitch("purl", "P", "purl", "p", 1);//Purl stitch
-
-export default StitchDictionary; 
+const knit = new StitchDictionary("knit", "K", "knit", "k", 1);//Knit stitch
+const purl = new StitchDictionary("purl", "P", "purl", "p", 1);//Purl stitch
+const k2tog = new StitchDictionary("knit 2 together", "k2tog", "increase - left leaning", "U", 2);
+const ssk =  new StitchDictionary("slip slip knit", "ssk", "increase - right leaning", "V", 2);
+const m1= new StitchDictionary("make one","m1", "make one",">", 1);
