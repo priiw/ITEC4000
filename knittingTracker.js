b7e05967181e318dea614893a8dbbd3eada6e313
priiw <48761109+priiw@users.noreply.github.com>
2019-03-29 16:45:40 -0400

    Update knittingTracker.js

--- knittingTracker.js
+++ knittingTracker.js
@@ -1,19 +1,47 @@
 //import defaultExport from './dictionary.js'
 
-let instruction = document.getElementById("textBox");
-const table = document.getElementById("tableTest");
+
+function splitCommas(line){
+    let stitches = line.split(",");
+    for (let j = 0; j < stitches.length; j++){
+        console.log("Stitch " + j + " : " + stitches[j].trim());
+    }
+}
+function splitLines(){
+    let instruction = document.getElementById("textBox").value;
+    let lines = instruction.split("\n");
+    for (let i = 0; i < lines.length; i++){
+        console.log("Line " + i + " : " + lines[i]);
+        splitCommas(lines[i]);
+    }
+}
+
+function createRow(rowId){
+    var row = document.createElement("TR");
+    row.setAttribute("id", rowId);
+    document.getElementById("knittingChart").appendChild(row);
+}
+
+function createCell(rowNumber){
+    var cell = document.createElement("TD");
+    var cellValue = document.createTextNode("cell");
+    cell.appendChild(cellValue);
+    document.getElementById(rowNumber).appendChild(cell);
+}
 
 function createChart() {
     var chart = document.createElement("TABLE");
     chart.setAttribute("id", "knittingChart");
     document.body.appendChild(chart);
 
-    var row = document.createElement("TR");
-    row.setAttribute("id", "rows");
-    document.getElementById("knittingChart").appendChild(row);
+    createRow("Row1");
+    createCell("Row1");
+    createRow("Row2");
+    createCell("Row2");
+}
 
-    var cell = document.createElement("TD");
-    var cellValue = document.createTextNode("cell");
-    cell.appendChild(cellValue);
-    document.getElementById("rows").appendChild(cell);
+function autoGrow (oField) {
+    if (oField.scrollHeight > oField.clientHeight) {
+        oField.style.height = oField.scrollHeight + "px";
+    }
 }
