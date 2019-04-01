74523810167d4f9aca6981c1a96f589d809aa49e
priiw <48761109+priiw@users.noreply.github.com>
2019-04-01 16:39:43 -0400

    Update knittingTracker.js

--- knittingTracker.js
+++ knittingTracker.js
@@ -1,23 +1,26 @@
 //import defaultExport from './dictionary.js'
-let existingChart = document.getElementById("knittingChart");
 
 function matchRegExp(stitch){
     stitch = "p5";
     let patt = /^[KP][0-9]$/i;
-    let result = stitch.match(patt);
-    if(result!=null) {
-        console.log(result);
-        let splitStitch = result.toString().split("");
-        for(let count = 0; count < splitStitch[1]; count++) {
-            console.log(splitStitch[0]);
-        }
-    }
+    return stitch.match(patt);
+
 }
 
-function splitCommas(line){
+function splitCommas(rowNumber, line){
     let stitches = line.split(",");
     for (let j = 0; j < stitches.length; j++){
         console.log("Stitch " + j + " : " + stitches[j].trim());
+        if(matchRegExp(stitches[j])!=null) {
+            let splitStitch = stitches[j].split("");
+            for (let count = 0; count < splitStitch[1]; count++) {
+                console.log(splitStitch[0]);
+                createCell("Row"+ rowNumber+1, splitStitch[0]);
+            }
+        }else{
+            createCell("Row"+ rowNumber+1, stitches[j]);
+
+        }
     }
 }
 function splitLines(){
@@ -25,49 +28,36 @@ function splitLines(){
     let lines = instruction.split("\n");
     for (let i = 0; i < lines.length; i++){
         console.log("Line " + i + " : " + lines[i]);
-        splitCommas(lines[i]);
+        createRow(i+1);
+        splitCommas(i, lines[i]);
     }
-}
-function createCaption(){
-    let caption = existingChart.createCaption();
-    caption.innerHTML = "Knitting Chart";
-}
-
-function deleteCaption(){
-    existingChart.deleteCaption();
 }
 
 function deleteChart(){
+    let existingChart = document.getElementById("knittingChart");
     while(existingChart!=null && existingChart.hasChildNodes()){
         existingChart.removeChild(existingChart.firstChild);
-        deleteCaption();
     }
 }
 
 function createRow(rowId){
     let row = document.createElement("TR");
-    row.setAttribute("id", rowId);
+    row.setAttribute("id", "Row" + rowId);
     document.getElementById("knittingChart").appendChild(row);
 }
 
-function createCell(rowNumber){
+function createCell(rowNumber,stitchValue){
     let cell = document.createElement("TD");
-    let cellValue = document.createTextNode("cell");
+    let cellValue = document.createTextNode(stitchValue);
     cell.appendChild(cellValue);
     document.getElementById(rowNumber).appendChild(cell);
 }
 
 function createChart() {
-    let chartCreation = document.createElement("TABLE");
-    chartCreation.setAttribute("id", "knittingChart");
-    chartCreation.setAttribute("align", "center");
-    document.body.appendChild(chartCreation);
-    createCaption();
-
-    createRow("Row1");
-    createCell("Row1");
-    createRow("Row2");
-    createCell("Row2");
+    deleteChart();
+    let chart = document.getElementById("knittingChart");
+    chart.createCaption().innerHTML = "Knitting Chart";
+    splitLines();
 }
 
 function autoGrow (oField) {
