3a18f72268bc5b4fcba65cebdef9402186598a6e
priiw <48761109+priiw@users.noreply.github.com>
2019-04-02 16:43:25 -0400

    Update knittingTracker.js

--- knittingTracker.js
+++ knittingTracker.js
@@ -1,50 +1,66 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="description" content="A page for tracking knitting or crocheting">
-    <link rel=stylesheet type="text/css" href="knittingTracker.css">
-    <link rel=stylesheet type="text/css" href="symbols.css">
+//import defaultExport from './dictionary.js'
+function matchRegExp(stitch){
+    stitch = "p5";
+    let patt = /^[KP][0-9]$/i;
+    return stitch.match(patt);
 
-    <title>Knitting Tracker</title>
-</head>
+}
 
-<body>
-<form action="">
-    <fieldset>
-        <legend>Pattern Information</legend>
-        Name:
-        <input type="text" name="name"><br />
-        Description:
-        <textarea name="description" rows="3" cols="30"></textarea><br />
-        Skill requirement:
-        <textarea name="skills" rows="3" cols="30"></textarea><br />
-        Materials:
-        <textarea name="materials" rows="3" cols="30"></textarea><br />
-        Gauge:
-        <input type="number" name="column" min="0"> x
-        <input type="number" name="row" min="0"> per
-        <input type="radio" name="gauge" value="4 inch"> 4"
-        <input type="radio" name="gauge" value="10 stitches"> 10 stitches<br />
-        Designer and Credit information:
-        <input type="text" name="credit"><br />
-        Copyright Statement:
-        <input type="text" name="copyright">
-    </fieldset>
-</form>
+function splitCommas(rowNumber, line){
+    let stitches = line.split(",");
+    for (let j = 0; j < stitches.length; j++){
+        console.log("Stitch " + j + " : " + stitches[j].trim());
+        if(matchRegExp(stitches[j])!=null) {
+            let splitStitch = stitches[j].split("");
+            for (let count = 0; count < splitStitch[1]; count++) {
+                console.log(splitStitch[0]);
+                createCell("Row"+ rowNumber+1, splitStitch[0]);
+            }
+        }else{
+            createCell("Row"+ rowNumber+1, stitches[j]);
 
-<textarea class="noscrollbars" id="textBox" onkeyup="autoGrow(this);" placeholder="Row 1: K2,P2,K2&#x0a;Row 2: K2,P2,K2"></textarea>
-<br />
-<button onclick="createChart()">Try it</button>
-<button onclick="splitLines()">split</button>
-<input type="submit" onclick="matchRegExp()" value="match" />
+        }
+    }
+}
+function splitLines(){
+    let instruction = document.getElementById("textBox").value;
+    let lines = instruction.split("\n");
+    for (let i = 0; i < lines.length; i++){
+        console.log("Line " + i + " : " + lines[i]);
+        createRow(i+1);
+        splitCommas(i, lines[i]);
+    }
+}
 
-<table id="knittingChart" align="center">
-    <caption>Knitting Chart</caption>
-</table>
-</body>
+function deleteChart(){
+    let existingChart = document.getElementById("knittingChart");
+    while(existingChart!=null && existingChart.hasChildNodes()){
+        existingChart.removeChild(existingChart.firstChild);
+    }
+}
 
+function createRow(rowId){
+    let row = document.createElement("TR");
+    row.setAttribute("id", "Row" + rowId);
+    document.getElementById("knittingChart").appendChild(row);
+}
 
-<script src="knittingTracker.js"></script>
-<script src="dictionary.js"></script>
-</html>
+function createCell(rowNumber,stitchValue){
+    let cell = document.createElement("TD");
+    let cellValue = document.createTextNode(stitchValue);
+    cell.appendChild(cellValue);
+    document.getElementById(rowNumber).appendChild(cell);
+}
+
+function createChart() {
+    deleteChart();
+    let chart = document.getElementById("knittingChart");
+    chart.createCaption().innerHTML = "Knitting Chart";
+    splitLines();
+}
+
+function autoGrow (oField) {
+    if (oField.scrollHeight > oField.clientHeight) {
+        oField.style.height = oField.scrollHeight + "px";
+    }
+}
